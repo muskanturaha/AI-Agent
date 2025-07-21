23a83839e1dc09a1372b363e8f908292df249d49
muskanturaha <100914952+muskanturaha@users.noreply.github.com>
2025-07-21 14:20:45 +0530

    Update slack_alert.py

--- slack_alert.py
+++ slack_alert.py
@@ -1,10 +1,11 @@
+# slack_alert.py
 import json
 import requests
 
 def send_slack_alert(webhook_url: str, alert: dict):
     text = (
-        "🚨 *Spike in Negative Sentiment*\n"
-        f"{alert['neg_high']} high‑urgency angry/confused messages "
-        f"in the last 30 min ({alert['fraction']} % of {alert['total']})."
+        f"🚨 *Spike in Negative Sentiment*\n"
+        f"{alert['neg_high']} high-urgency angry/sad messages in the last 5 mins "
+        f"({alert['percent']}% of {alert['total']})."
     )
     requests.post(webhook_url, data=json.dumps({"text": text}))
